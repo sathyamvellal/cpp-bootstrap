a8b647c8f8f3ccd5066aa9c5007a69349607af6c
Sathyam Vellal <sathyam.vellal@gmail.com>
2017-11-06 20:37:07 -0800

    Reorganising to hpc-expriments idea

--- common/src/hello.cpp
+++ common/src/hello.cpp
@@ -1,9 +1,9 @@
 //
-// Created by Sathyam Vellal on 12/10/2017.
+// Created by Sathyam Vellal on 24/10/2017.
 //
 
 #include <iostream>
-#include <hello.h>
+#include "hello.h"
 
 void hello() {
     std::cout << "Hello, World!" << std::endl;
